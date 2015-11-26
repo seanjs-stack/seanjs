f2e4f9908537e647e3bd3e6424d5fff74db20156
Massad <contact@omarmassad.com>
2015-11-26 05:55:13 +0200

    Fix upload image

--- modules/users/server/controllers/users/user.profile.server.controller.js
+++ modules/users/server/controllers/users/user.profile.server.controller.js
@@ -118,54 +118,62 @@ exports.update = function(req, res, next) {
  * Update profile picture
  */
 exports.changeProfilePicture = function(req, res) {
-  var user = req.user;
-  var message = null;
-  var oldImage = user.profileImageURL;
 
-  if (user) {
-    if (!req.file) {
-      return res.status(400).send({
-        message: 'Error occurred while uploading profile picture'
-      });
-    } else {
-
-      user.profileImageURL = req.file.filename;
-
-      user.save().then(function(saved) {
-        if (!saved) {
-          return res.status(400).send({
-            message: errorHandler.getErrorMessage(saved)
-          });
-        } else {
-
-          if (oldImage) {
-            try {
-              var stats = fs.lstatSync('./public/uploads/users/profile/' + oldImage);
-              if (stats.isFile()) {
-                fs.unlinkSync('./public/uploads/users/profile/' + oldImage);
-              }
-            } catch (e) {
-              console.log('Unable to delete the old image');
-            }
-
-          }
-
-          req.login(user, function(err) {
-            if (err) {
-              res.status(400).send(err);
-            } else {
-              res.json(user);
-            }
-          });
-        }
-      });
+  User.findOne({
+    where: {
+      id: req.user.id
     }
+  }).then(function(user) {
 
-  } else {
-    res.status(400).send({
-      message: 'User is not signed in'
+    if (user) {
+      if (!req.file) {
+        return res.status(400).send({
+          message: 'Error occurred while uploading profile picture'
+        });
+      } else {
+
+        var oldImage = user.profileImageURL;
+
+        user.profileImageURL = req.file.filename;
+
+        user.save().then(function(saved) {
+          if (!saved) {
+            return res.status(400).send({
+              message: errorHandler.getErrorMessage(saved)
+            });
+          } else {
+            if (oldImage) {
+              try {
+                var stats = fs.lstatSync('./public/uploads/users/profile/' + oldImage);
+                if (stats.isFile()) {
+                  fs.unlinkSync('./public/uploads/users/profile/' + oldImage);
+                }
+              } catch (e) {
+                console.log('Unable to delete the old image');
+              }
+            }
+
+            req.user.profileImageURL = user.profileImageURL;
+            res.json(user);
+          }
+        }).catch(function(err) {
+          console.log('err', err);
+          return res.status(400).send({
+            message: errorHandler.getErrorMessage(err)
+          });
+        });
+      }
+
+    }
+  }).catch(function(err) {
+    console.log('err', err);
+    return res.status(400).send({
+      message: errorHandler.getErrorMessage(err)
     });
-  }
+  });
+
+
+
 };
 
 exports.getProfile = function(req, res) {
