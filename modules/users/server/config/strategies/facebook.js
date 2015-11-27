28502d43d17ae6a0bcaf659acc78e25cc6d242f4
Omar Massad <contact@omarmassad.com>
2015-11-28 00:53:28 +0200

    Add https to the image url

--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
         displayName: profile.displayName,
         email: profile.emails ? profile.emails[0].value : undefined,
         username: profile.username || generateUsername(profile),
-        profileImageURL: (profile.id) ? '//graph.facebook.com/' + profile.id + '/picture?type=large' : undefined,
+        profileImageURL: (profile.id) ? 'https://graph.facebook.com/' + profile.id + '/picture?type=large' : undefined,
         provider: 'facebook',
         providerIdentifierField: 'id',
         providerData: providerData
