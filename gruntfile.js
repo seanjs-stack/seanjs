cc1602eaf69e54f5f35d1eff71a832cd5779ffc7
Massad <contact@omarmassad.com>
2015-11-27 04:05:09 +0200

    Reduce the timeout

--- gruntfile.js
+++ gruntfile.js
@@ -252,7 +252,7 @@ module.exports = function(grunt) {
     var server = app.start(function() {
       setTimeout(function() { //for sequelize.sync()
         done();
-      }, 10000);
+      }, 100); //10000
 
     });
   });
